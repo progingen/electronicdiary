8d1d7c0fecea3f3d165c543001786f1b9d969c2a
Сергей Спиридонов <sergey.spr@gmail.com>
2014-03-11 18:22:15 +0400

    added birtday and group fields.

--- src/StudentList.java
+++ src/StudentList.java
@@ -1,4 +1,6 @@
 import java.io.*;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.ArrayList;
 
 import javax.swing.*;
@@ -9,23 +11,23 @@ import java.awt.*;
 
 import structures.*;
 import components.*;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.InputMethodListener;
+import java.awt.event.InputMethodEvent;
 
 
 public class StudentList extends JFrame implements ListSelectionListener {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 		
 	private ArrayList<Student> students;
 	
-	// GUI components
 	private CustomTextField searchTextField;
+	private String previousSearchString = "";
 	private JList studentsList;
-	private JScrollPane listScrollPane;
 	
 	private JLabel nameLabel;
 	
@@ -53,16 +55,62 @@ public class StudentList extends JFrame implements ListSelectionListener {
 		headerPanel.setLayout(new BoxLayout(headerPanel, BoxLayout.X_AXIS));
 		
 		searchTextField = new CustomTextField(30);
-		searchTextField.addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyPressed(KeyEvent arg0) {
+		searchTextField.addInputMethodListener(new InputMethodListener() {
+			public void caretPositionChanged(InputMethodEvent arg0) {
 				
+				nameLabel.setText(arg0.paramString());
+			}
+			
+			public void inputMethodTextChanged(InputMethodEvent arg0) {
 				
+				nameLabel.setText(searchTextField.getText());
 			}
 		});
 		searchTextField.setFont(new Font("Segoe UI", Font.PLAIN, 13));
 		searchTextField.setBorder(new EmptyBorder(5, 5, 5, 5));
 		searchTextField.setPlaceholder("Поиск...");
+		/*searchTextField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				
+				String stringToFind = searchTextField.getText();
+				
+				if (previousSearchString.equals(stringToFind))
+					return;
+				
+				String[] names = null;
+				
+				if (stringToFind.equals(""))
+				{
+					names = new String[students.size()];
+					
+					for (int i = 0; i < students.size(); i++)
+					{
+						Student student = students.get(i);
+						names[i] = student.secondName + " " + student.firstName + " " + student.middleName;
+					}
+				}
+				else
+				{
+					ArrayList<String> tempArray = new ArrayList<String>();
+					
+					for (int i = 0; i < students.size(); i++)
+					{						
+						Student student = students.get(i);
+						String name =  student.secondName + " " + student.firstName + " " + student.middleName;
+							
+						if (FindSubstring(name, stringToFind))
+							tempArray.add(name);
+					}
+					
+					names = new String[tempArray.size()];
+					tempArray.toArray(names);
+				}
+				
+				UpdateList(names);
+				previousSearchString = stringToFind;
+			}
+		});*/
 		
 		headerPanel.add(searchTextField);
 		rootPanel.add(headerPanel, BorderLayout.NORTH);
@@ -75,12 +123,11 @@ public class StudentList extends JFrame implements ListSelectionListener {
 		for (int i = 0; i < students.size(); i++)
 		{
 			names[i] = 
-				(i + 1) + " - " +
 				students.get(i).secondName + " " + 
 				students.get(i).firstName + " " + 
 				students.get(i).middleName;
 		}
-		
+			
 		studentsList = new JList(names);
 		studentsList.setBorder(new EmptyBorder(5, 5, 5, 5));
 		studentsList.setFont(new Font("Segoe UI", Font.PLAIN, 13));
@@ -88,15 +135,16 @@ public class StudentList extends JFrame implements ListSelectionListener {
 		studentsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		studentsList.setSelectedIndex(0);
 		studentsList.addListSelectionListener(this);
-		
-        listScrollPane = new JScrollPane(studentsList);
-        listScrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
-        rootPanel.add(listScrollPane, BorderLayout.WEST);
+			
+		JScrollPane listScrollPane = new JScrollPane(studentsList);
+		listScrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+	    listScrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+	    rootPanel.add(listScrollPane, BorderLayout.WEST);
 	}
 	
-	private void UpdateList(ArrayList<Integer> indices)
+	private void UpdateList(String[] names)
 	{
-		
+		studentsList.setListData(names);
 	}
 	
 	private void ShowStudentInfo(JPanel rootPanel)
@@ -106,19 +154,25 @@ public class StudentList extends JFrame implements ListSelectionListener {
         
         studentInfo.setLayout(studentInfoLayout);
         
-        JLabel nameLabelHint = new JLabel("Имя:");
-        nameLabelHint.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-        studentInfoLayout.putConstraint(SpringLayout.NORTH, nameLabelHint, 10, SpringLayout.NORTH, studentInfo);
-        studentInfoLayout.putConstraint(SpringLayout.WEST, nameLabelHint, 10, SpringLayout.WEST, studentInfo);
-        studentInfo.add(nameLabelHint);
-        
         nameLabel = new JLabel("sdfdf");
-        nameLabel.setFont(new Font("Segoe UI", Font.PLAIN, 12));
-        studentInfoLayout.putConstraint(SpringLayout.NORTH, nameLabel, 0, SpringLayout.NORTH, nameLabelHint);
-        studentInfoLayout.putConstraint(SpringLayout.WEST, nameLabel, 35, SpringLayout.EAST, nameLabelHint);
+        studentInfoLayout.putConstraint(SpringLayout.NORTH, nameLabel, 10, SpringLayout.NORTH, studentInfo);
+        studentInfoLayout.putConstraint(SpringLayout.WEST, nameLabel, 10, SpringLayout.WEST, studentInfo);
+        nameLabel.setFont(new Font("Segoe UI", Font.ITALIC, 20));
         studentInfo.add(nameLabel);
         
         rootPanel.add(studentInfo, BorderLayout.CENTER);
+        
+        JLabel birthdayLabel = new JLabel("Дата рождения: 18.06.1994г. (19 лет)");
+        birthdayLabel.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+        studentInfoLayout.putConstraint(SpringLayout.NORTH, birthdayLabel, 6, SpringLayout.SOUTH, nameLabel);
+        studentInfoLayout.putConstraint(SpringLayout.WEST, birthdayLabel, 0, SpringLayout.WEST, nameLabel);
+        studentInfo.add(birthdayLabel);
+        
+        JLabel label = new JLabel("Группа: 205");
+        label.setFont(new Font("Segoe UI", Font.PLAIN, 13));
+        studentInfoLayout.putConstraint(SpringLayout.NORTH, label, 6, SpringLayout.SOUTH, birthdayLabel);
+        studentInfoLayout.putConstraint(SpringLayout.WEST, label, 0, SpringLayout.WEST, nameLabel);
+        studentInfo.add(label);
 	}
 	
 	// Listens to the list
@@ -129,5 +183,40 @@ public class StudentList extends JFrame implements ListSelectionListener {
         
         Student student = this.students.get(selectedIndex);
         this.nameLabel.setText(student.secondName + " " + student.firstName + " " + student.middleName);
+    }
+    
+    private boolean FindSubstring(String src, String key)
+    {
+    	if (src.length() < key.length())
+    		return false;
+    	
+    	src = src.toLowerCase();
+    	key = key.toLowerCase();
+    	
+    	int keyLength = key.length();
+    	int length = src.length() - key.length();
+    	
+    	for (int i = 0; i < length; )
+    	{
+    		if (src.charAt(i) == key.charAt(0))
+    		{
+    			i++;
+    			
+    			for (int j = 1; j < keyLength || i < length; j++)
+    			{
+    				if (src.charAt(i) != key.charAt(j))
+    					break;
+
+    				if (j + 1 == keyLength)
+    					return true;
+    					
+    				i++;
+    			}
+    		}
+    		else 
+    			i++;
+    	}
+    	
+    	return false;
     }
 }
