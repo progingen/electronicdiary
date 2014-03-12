26f01b397a2c214ae97beb84f8a1026ab4a19ffb
Сергей Спиридонов <sergey.spr@gmail.com>
2014-03-12 16:32:40 +0400

    я сделал то-то

--- src/form3.java
+++ src/form3.java
@@ -4,11 +4,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JComboBox;
 
 
 public class form3 extends JFrame {
-
-	private JPanel contentPane;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -31,11 +34,24 @@ public class form3 extends JFrame {
 	 */
 	public form3() {
 		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setBounds(100, 100, 573, 372);
+		getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("\u0418\u043C\u044F");
+		label.setBounds(10, 11, 46, 14);
+		getContentPane().add(label);
+		
+		textField = new JTextField();
+		textField.setBounds(164, 8, 175, 20);
+		getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JLabel label_1 = new JLabel("\u0421\u043A\u043E\u043B\u044C\u043A\u043E \u0441\u043E\u0431\u0430\u043A?");
+		label_1.setBounds(10, 36, 94, 14);
+		getContentPane().add(label_1);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(164, 33, 175, 20);
+		getContentPane().add(comboBox);
 	}
-
 }
